02ac82f068738f02fed5663f702dded91a9b3e15
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-12-17 15:08:45 +0530

    build files

---
+++ app/build/app/src/auth/user/user.model.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.User = void 0;
+const mongoose_1 = __importDefault(require("mongoose"));
+const schema = new mongoose_1.default.Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+}, {
+    toJSON: {
+        transform(doc, ret) {
+            ret.id = ret._id;
+            delete ret._id;
+            delete ret.password;
+        },
+    },
+});
+exports.User = mongoose_1.default.model("User", schema);
