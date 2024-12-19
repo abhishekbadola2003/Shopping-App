c94aa9ff4f675faecca75d8638435c616e8c31f5
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-12-19 15:31:38 +0530

    updates in  user service

--- app/src/auth/user/user.service.ts
+++ app/src/auth/user/user.service.ts
@@ -1,13 +1,13 @@
 import { UserModel } from "@shoppingapp/common";
 import { User } from "./user.model";
-import { CreateUserDtos } from "../dtos/auth.dtos";
+import { AuthDto } from "../dtos/auth.dtos";
 
 export class UserService {
   constructor(public userModel: UserModel) {}
-  async create(createUserDtos: CreateUserDtos) {
+  async create(AuthDto: AuthDto) {
     const user = new this.userModel({
-      email: createUserDtos.email,
-      password: createUserDtos.password,
+      email: AuthDto.email,
+      password: AuthDto.password,
     });
     return await user.save();
   }
