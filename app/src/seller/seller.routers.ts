5d00993e32f869769e24d8ecf2bb1b64d8b047dd
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-12-22 22:39:17 +0530

    enhancements in seller router

--- app/src/seller/seller.routers.ts
+++ app/src/seller/seller.routers.ts
@@ -1,9 +1,27 @@
 import { Response, Request, NextFunction, Router } from "express";
+import {
+  BadRequestError,
+  Uploader,
+  UploaderMiddlewareOptions,
+} from "@shoppingapp/common";
+
+const uploader = new Uploader();
+const middlewareOptions: UploaderMiddlewareOptions = {
+  types: ["image/png", "image/jpeg"],
+  fieldName: "image",
+};
+
+const multipleFilesMiddleware = uploader.uploadMultipleFiles(middlewareOptions);
 
 const router = Router();
 router.post(
   "/product/new",
   async (req: Request, res: Response, next: NextFunction) => {
     const { title, price } = req.body;
+
+    if (!req.files) return next(new BadRequestError("images are required."));
+
+    if (req.uploaderError)
+      return next(new BadRequestError(req.uploaderError.message));
   }
 );
